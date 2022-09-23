216f33397174c41857173e74f998d0ea3418ce28
Prof. Chico Freitas <chicoo.freitas84@gmail.com>
2022-09-23 17:16:24 -0300

    add Outlet to dashboard main page

--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -197,7 +197,7 @@ const Main = () => {
               <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
                 {/* Replace with your content */}
                 <div className="px-4 py-6 sm:px-0">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                  <Outlet />
                 </div>
                 {/* /End replace */}
               </div>
